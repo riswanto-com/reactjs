3ce1191e0f05d6c27e9a19a6e54c6570f3f61ba9
Riswanto <37138545+wansr87@users.noreply.github.com>
2020-05-13 12:53:43 +0700

    Updates

---
+++ precache-manifest.2a3abfdf27a25dd1c9a6b9388a96cbeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258510459f91cdeb5ff05f8dbcf0e190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c77daed4e533f51ecd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc28577de47be5c59a99",
+    "url": "/static/js/2.9526ae4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9526ae4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c77daed4e533f51ecd",
+    "url": "/static/js/main.10e577a7.chunk.js"
+  },
+  {
+    "revision": "f105b50335e8dfae7864",
+    "url": "/static/js/runtime-main.583156a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
